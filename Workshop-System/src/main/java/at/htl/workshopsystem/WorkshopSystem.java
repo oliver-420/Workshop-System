b72cd93a2f9e9aedd8e9fe51194fd666c275b6a6
oliver-420 <oliver.daxinger@gmail.com>
2023-06-06 22:01:13 +0200

    Added tasks.fxml, worked on binding buttons on home

--- Workshop-System/src/main/java/at/htl/workshopsystem/WorkshopSystem.java
+++ Workshop-System/src/main/java/at/htl/workshopsystem/WorkshopSystem.java
@@ -6,18 +6,24 @@ import at.htl.workshopsystem.model.Car;
 import at.htl.workshopsystem.model.Mechanic;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
 public class WorkshopSystem extends Application {
+    public static Stage stage = null;
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(WorkshopSystem.class.getResource("home.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Parent root = FXMLLoader.load(getClass().getResource("home.fxml"));
+        Scene scene = new Scene(root);
+        stage.initStyle(StageStyle.UNDECORATED);
+        stage.setTitle("Home");
         stage.setScene(scene);
+        this.stage = stage;
+        //MetaData.parent = root;
         stage.show();
     }
 
--- Workshop-System/src/main/java/at/htl/workshopsystem/view/controller/HomeController.java
+++ Workshop-System/src/main/java/at/htl/workshopsystem/view/controller/HomeController.java
@@ -1,14 +1,66 @@
 package at.htl.workshopsystem.view.controller;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Objects;
+import java.util.ResourceBundle;
 
 public class HomeController {
     @FXML
     private Label welcomeText;
 
     @FXML
+    private Button btn_customers;
+    @FXML
+    private Button btn_tasks;
+
+    @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to our Workshop-System");
+    }
+
+    @FXML
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        btn_customers.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                changeScene(event, "customers.fxml", "Customers");
+            }
+        });
+
+        btn_tasks.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                changeScene(event, "tasks.fxml", "Tasks");
+            }
+        });
+    }
+
+    //change scene to customers or tasks
+    public void changeScene(ActionEvent event, String fxmlFile, String title){
+        Parent root = null;
+
+        try{
+            FXMLLoader loader = new FXMLLoader(HomeController.class.getResource(fxmlFile));
+            root = loader.load();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setTitle(title);
+        stage.setScene(new Scene(root));
+        stage.show();
     }
 }
