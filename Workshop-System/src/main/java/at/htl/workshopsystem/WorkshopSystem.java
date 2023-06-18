f52bf60815a4b91433f9b29a351e6693bbff2eb9
Linus Pc <linusnestler@gmail.com>
2023-06-18 21:45:20 +0200

    on start big window, but commented out

--- Workshop-System/src/main/java/at/htl/workshopsystem/WorkshopSystem.java
+++ Workshop-System/src/main/java/at/htl/workshopsystem/WorkshopSystem.java
@@ -5,10 +5,12 @@ import atlantafx.base.theme.CupertinoLight;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -19,6 +21,13 @@ public class WorkshopSystem extends Application {
     public void start(Stage stage) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("home.fxml"));
         Scene scene = new Scene(root);
+        Screen screen = Screen.getPrimary();
+        Rectangle2D bounds = screen.getVisualBounds();
+
+        /*stage.setX(bounds.getMinX());
+        stage.setY(bounds.getMinY());
+        stage.setWidth(bounds.getWidth());
+        stage.setHeight(bounds.getHeight());*/
         stage.setTitle("Home");
         stage.setScene(scene);
         WorkshopSystem.stage = stage;
