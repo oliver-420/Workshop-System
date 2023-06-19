544fe9898920cdb168e22e160a01f055b798c3ae
Linus <Linusnestler@gmail.com>
2023-06-19 14:18:08 +0200

    added test dialog for carsearch on create Task

--- Workshop-System/src/main/java/at/htl/workshopsystem/view/controller/AddTaskController.java
+++ Workshop-System/src/main/java/at/htl/workshopsystem/view/controller/AddTaskController.java
@@ -6,16 +6,17 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.DialogPane;
-import javafx.scene.control.TextField;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
 import javafx.stage.Popup;
+import javafx.stage.Stage;
 
 
 public class AddTaskController {
@@ -43,13 +44,27 @@ public class AddTaskController {
     private void setCarNode(){
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
         try {
-            Node node =  FXMLLoader.load(getClass().getResource("/at/htl/workshopsystem/" + "carSearch.fxml"));
+           /* Node node =  FXMLLoader.load(getClass().getResource("/at/htl/workshopsystem/" + "carSearch.fxml"));
             AnchorPane main = (AnchorPane) node.lookup("#pane");
-            DialogPane pane = new DialogPane();
+            DialogPane pane = new DialogPane(500, 500);
             pane.getChildren().add(node);
             alert.setHeaderText("Auto suchen");
             alert.setDialogPane(pane);
-            alert.showAndWait();
+            alert.showAndWait();*/
+
+            Stage stage = new Stage();
+            stage.initModality(Modality.APPLICATION_MODAL);
+
+            Label label = new Label("text");
+
+            VBox layout = new VBox(label);
+            layout.setAlignment(Pos.CENTER);
+
+            Scene scene = new Scene(layout, 200, 100);
+
+            stage.setTitle("Dialog");
+            stage.setScene(scene);
+            stage.show();
 
         }
         catch (Exception e){
